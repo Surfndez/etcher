6ff30fef6dd1758527a5a1fa12b56016a2f9da7d
Lorenzo Alberto Maria Ambrosi <lorenzoa@balena.io>
2019-05-06 17:36:12 +0200

    Add clean-shrinkwrap script to postshrinkwrap step
    Change-type: patch
    Changelog-entry: Add clean-shrinkwrap script to postshrinkwrap step
    Signed-off-by: Lorenzo Alberto Maria Ambrosi <lorenzoa@balena.io>

---
+++ scripts/clean-shrinkwrap.js
@@ -0,0 +1,129 @@
+/**
+ * This script is in charge of generating the `shrinkwrap` file.
+ *
+ * `npm shrinkwrap` has a bug where it will add optional dependencies
+ * to `npm-shrinkwrap.json`, therefore causing errors if these optional
+ * dependendencies are platform dependent and you then try to build
+ * the project in another platform.
+ *
+ * As a workaround, we keep a list of platform dependent dependencies in
+ * the `platformSpecificDependencies` property of `package.json`,
+ * and manually remove them from `npm-shrinkwrap.json` if they exist.
+ *
+ * See: https://github.com/npm/npm/issues/2679
+ */
+
+/* eslint-disable lodash/prefer-lodash-method */
+/* eslint-disable no-magic-numbers */
+/* eslint-disable no-undefined */
+
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const _ = require('lodash')
+
+const JSON_INDENT = 2
+const SHRINKWRAP_FILENAME = path.join(__dirname, '..', 'npm-shrinkwrap.json')
+
+const packageInfo = require('../package.json')
+const shrinkwrap = require('../npm-shrinkwrap.json')
+
+/**
+ * @summary Delete a dependency
+ *
+ * @param {*} keys - Keys reference
+ * @param {*} tree - Tree to iterate over
+ * @param {*} onNode - Function to apply
+ * @param {*} parentName - Name of dependent
+ * @param {*} depType - Dependency type (dependencies|requires)
+ *
+ * @example
+ * removeDependency(keys, tree, onNode, parentName)
+ */
+const removeDependency = (keys, tree, onNode, parentName, depType) => {
+  let name = null
+  for (let index = 0; index < keys.length; index += 1) {
+    name = keys[index]
+
+    // Check for this dependency to still exist,
+    // as a node might have been removed just before this iteration
+    if (tree[depType][name]) {
+      onNode(tree, parentName || tree.name, name, tree[depType][name], depType)
+
+      // Check that the walking function didn't remove the dependency
+      // if so, skip it and continue with the next one
+      if (tree[depType][name] && tree[depType][name][depType]) {
+        traverseDeps(tree[depType][name], onNode, name)
+      }
+    }
+  }
+}
+
+/**
+ * @summary Traverse a shrinkwrap tree and call
+ * a given function on each dependency
+ * @param {Object} tree - shrinkwrap
+ * @param {Function} onNode - callback({Object} parent, {String} parentName, {String} name, {Object} info)
+ * @param {String} [parentName] - name of dependent (used internally)
+ * @example
+ * traverseDeps(shrinkwrap, (parent, parentName, name, info) => {
+ *   // ...
+ * })
+ */
+const traverseDeps = (tree, onNode, parentName) => {
+  if (!tree.dependencies && !tree.requires) {
+    return
+  }
+
+  const depsKeys = Object.keys(tree.dependencies)
+  removeDependency(depsKeys, tree, onNode, parentName, 'dependencies')
+
+  if (_.isObject(tree.requires)) {
+    const reqsKeys = Object.keys(tree.requires)
+    removeDependency(reqsKeys, tree, onNode, parentName, 'requires')
+  }
+}
+
+console.log('Cleaning shrinkwrap...')
+
+// Walk the generated shrinkwrap tree & apply modifications if necessary
+traverseDeps(shrinkwrap, (parent, parentName, name, info, depType) => {
+  // If this dependency depends on a "blacklisted" optional
+  // dependency; remove it from the shrinkwrap
+  if (packageInfo.platformSpecificDependencies.includes(name)) {
+    console.log(' - Removing "%s" from "%s"', name, parentName)
+    parent.dependencies[name] = undefined
+    Reflect.deleteProperty(parent.dependencies, name)
+
+    // Check that we're not in the root object
+    // before removing the "requires" property
+    if (_.isObject(parent.requires)) {
+      parent.requires[name] = undefined
+      Reflect.deleteProperty(parent.requires, name)
+    }
+    return
+  }
+
+  // Ensure the `resolved` field contains a HTTPS registry URL,
+  // as some versions of npm apparently fall back to HTTP on some platforms
+  // under some circumstances
+  if (/^http:\/\//.test(info.resolved)) {
+    info.resolved = info.resolved.replace(/^http/, 'https')
+  }
+})
+
+// Generate the new shrinkwrap JSON
+const shrinkwrapJson = `${JSON.stringify(shrinkwrap, null, JSON_INDENT)}\n`
+
+// Write back the modified npm-shrinkwrap.json
+fs.writeFile(SHRINKWRAP_FILENAME, shrinkwrapJson, (error) => {
+  if (error) {
+    console.log(`[ERROR] Couldn't write shrinkwrap file: ${error.stack}`)
+    process.exit(1)
+  } else {
+    console.log(`[OK] Wrote shrinkwrap file to ${path.relative(__dirname, SHRINKWRAP_FILENAME)}`)
+  }
+})
+
+console.log('')
